2a53fc4c77d5282c107847620f21f2d31db55686
Nate Festinger <nathaniel.festinger@gmail.com>
2021-07-01 09:59:55 -0700

    Clean up a bit

--- src/util/db.js
+++ src/util/db.js
@@ -1,10 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-/* configured via https://firebase.google.com/docs/web/setup
-*/
 
+// configure firebase (https://firebase.google.com/docs/web/setup)
 const config = {
-  apiKey: "AIzaSyD7NUVfrImccSo8FuCBG7bXVk0oLFqgE-k",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "yardzen-demo.firebaseapp.com",
   databaseURL: "https://yardzen-demo.firebaseio.com",
   projectId: "yardzen-demo",
@@ -13,7 +12,7 @@ const config = {
   appId: "1:509183652730:web:ba2208f7d8e0882f009cc3"
 };
 
-// don't initialize twice: https://stackoverflow.com/questions/43331011
+// don't initialize twice (https://stackoverflow.com/questions/43331011)
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
 }
